5fdec49baf52f05ffa578458e0450daac45cd028
Mitchell Browne <mitchellbrowne@Mitchells-MacBook-Air-2.local>
2020-04-04 09:24:36 +1100

    Added initial basic seed data

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :email, :presence => true, :uniqueness => true
+  
   belongs_to :company, :optional => true
   has_many :ads
   has_many :galleries
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,27 @@
 User.destroy_all
-u1 = User.create :name => 'Lola', :email => 'lola@gmail.com', :password => 'fish'
-
+u1 = User.create :name => 'Josh', :email => 'josh@gmail.com', :password => 'fish'
+u2 = User.create :name => 'Frank', :email => 'frank@gmail.com', :password => 'fish', :admin => true
+u3 = User.create :name => 'Azi', :email => 'azi@gmail.com', :password => 'fish'
+u4 = User.create :name => 'Mitch', :email => 'mitch@gmail.com', :password => 'fish'
 puts "#{User.count} users created"
+
+Company.destroy_all
+c1 = Company.create :name => 'Apple', :description => 'Tech unicorn', :image => 'https://www.brandchannel.com/wp-content/uploads/2018/02/apple-rainbow-logo-original.jpg'
+c2 = Company.create :name => 'Hinge', :description => 'Dating app', :image => 'https://globaldatinginsights.com/wp-content/uploads/2016/10/Screen-Shot-2016-10-11-at-15.44.59.png'
+c3 = Company.create :name => 'Uber Eats', :description => 'Food Delivery', :image => 'https://storage.googleapis.com/ubereats/UE-FB-Post.png'
+puts "#{Company.count} companies created"
+
+Ad.destroy_all
+a1 = Ad.create :name => 'AirPods Pro - Snap', :image => image_url('Apple_AirPods_Pro_Snap.png'), :ad_type => 'TV'
+a2 = Ad.create :name => 'AirPods - Bounce', :image => image_url('Apple_AirPods_Bounce.png'), :ad_type => 'TV'
+a3 = Ad.create :name => 'IPhone 11 Pro - Its Tough Out There', :image => image_url('Apple_ItsToughOutThere_IPhone11Pro.png'), :ad_type => 'TV'
+
+a4 = Ad.create :name => 'Kim and Sharon', :image => 'https://i.dailymail.co.uk/1s/2019/11/24/13/21402890-0-image-a-26_1574601178356.jpg', :ad_type => 'Instagram'
+a5 = Ad.create :name => 'Barnsey and Farnsey', :image => 'https://res.cloudinary.com/themusicau/image/upload/c_fill,f_auto,h_459,w_768/standard/news_19/barnesy_farnsy_anhsy_h_1019', :ad_type => 'TV'
+puts "#{Ad.count} ads created"
+
+Gallery.destroy_all
+g1 = Gallery.create :name => 'Apple 2019 Ads', :category => 'Tech Consumer'
+g2 = Gallery.create :name => 'UberEats 2019 Ads', :category => 'Delivery'
+
+puts "#{Gallery.count} galleries created"
