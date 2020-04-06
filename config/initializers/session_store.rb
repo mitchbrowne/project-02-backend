608ed77581a79057bdce10f6a9d6f91f99cbf6a9
frankda <suyunyi@gmail.com>
2020-04-06 16:32:05 +1000

    update production setting

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 if Rails.env === 'production' 
     # modify domain name fater deployment
-    Rails.application.config.session_store :cookie_store, key: '_compaign-app', domain: 'your-frontend-domain'
+    Rails.application.config.session_store :cookie_store, key: '_compaign-markt', domain: 'https://campaign-markt.herokuapp.com/'
   else
-    Rails.application.config.session_store :cookie_store, key: '_compaign-app' 
+    Rails.application.config.session_store :cookie_store, key: '_compaign-markt' 
   end
