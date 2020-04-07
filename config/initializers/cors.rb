73d3eb35182130c1a0858ee187240743fdcf60c1
Mitchell Browne <mitchellbrowne@Mitchells-MacBook-Air-2.local>
2020-04-07 14:11:22 +1000

    Updated cors (to localhost:3000) and added seeds

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,10 +2,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins '*'   #change after deployment
+      origins 'http://localhost:3000'   #change after deployment
       resource '*',
         :headers => :any,
         :methods => %i( get post put patch delete options head ),
-        :credentials => false # set to true after deployment
+        :credentials => true # set to true after deployment
     end
   end
