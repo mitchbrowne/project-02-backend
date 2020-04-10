f8cbc0b8b7efe2d97fde7952a13389c397947cf5
frankda <suyunyi@gmail.com>
2020-04-10 16:20:31 +1000

    change origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,5 +8,12 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
         :methods => %i( get post put patch delete options head ),
         :credentials => true
     end
+
+    allow do
+      origins 'https://campaign-ads-markt.netlify.com' 
+      resource '*',
+        :headers => :any,
+        :methods => %i( get post put patch delete options head ),
+        :credentials => true
+    end
   end
-  
