f443f6aadfddf6e0b1c80f7030b4764e17422280
Mitchell Browne <mitchellbrowne@Mitchells-MacBook-Air-2.local>
2020-04-20 12:06:10 +1000

    Updated origin cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,18 +2,18 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'localhost:3000', 'https://campaign-ads-markt.netlify.app/'   #change after deployment
+      origins 'localhost:3000', 'https://campaign-ads-markt.netlify.com'   #change after deployment
       resource '*',
         :headers => :any,
         :methods => %i( get post put patch delete options head ),
         :credentials => true
     end
 
-    allow do
-      origins 'https://campaign-ads-markt.netlify.com' 
-      resource '*',
-        :headers => :any,
-        :methods => %i( get post put patch delete options head ),
-        :credentials => true
-    end
+    # allow do
+    #   origins 'https://campaign-ads-markt.netlify.com'
+    #   resource '*',
+    #     :headers => :any,
+    #     :methods => %i( get post put patch delete options head ),
+    #     :credentials => true
+    # end
   end
